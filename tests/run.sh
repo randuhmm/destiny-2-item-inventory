d6241d3ce30199cacff9d8eee3417b71ed355460
Jonny Morrill <jrmorrill@gmail.com>
2018-06-13 21:40:32 -0700

    fixing validation script

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+
 errors=0
 
 basedir="$(dirname $0)"
@@ -7,17 +7,27 @@ validate_script="$basedir/validate_csv.sh"
 
 cat "$basedir/../data/weapons.csv" | "$validate_script" \
     12 \
-    "r'[0-9]+'" \
+    "r'\d+'" \
     "r'.+'" \
     "r'.+'" \
     "r'Energy|Kinetic|Power'" \
-    "r'Exotic|Legendary'"
-[ "$?" -ne 0 ] || {
+    "r'Exotic|Legendary'" \
+    "r'\d*'" \
+    "r'\d*'" \
+    "r'\d*'" \
+    "r'\d*'" \
+    "r'\d*'" \
+    "r'\d*'" \
+    "r'^$|^.*\.(jpg|png)$'"
+[ "$?" -ne 0 ] && {
     echo "Validation failed for 'weapons.csv'" 2>&1
     let errors+=1
 }
 
 [ "$errors" -gt 0 ] && {
-    echo "Found $errors validation error(s)"
+    echo "Found $errors validation error(s)" 2>&1
     exit 1
+} || {
+    echo "Validation successful" 2>&1
+    exit 0
 }
--- tests/validate_csv.sh
+++ tests/validate_csv.sh
@@ -5,23 +5,39 @@ RE_LIST=$(printf ",%s" "${@:2}")
 RE_LIST=${RE_LIST:1}
 read -r -d '' CMD <<EOF
 import csv, sys, re
-reader = csv.reader(sys.stdin, quotechar='"')
+quote_char = '"'
+delim_char = ','
+RED = "\033[1;31m"
+RESET = "\033[1;0m"
+def writeout(s):
+  sys.stdout.write(s)
+reader = csv.reader(sys.stdin, quotechar=quote_char)
 i = 1
 rec = []
 for r in [$RE_LIST]:
-    rec.append(re.compile("^%s$" % r))
+    if r[0] != '^': r = '^' + r
+    if r[-1] != '$': r = r + '$'
+    rec.append(re.compile(r))
 for row in reader:
     if len(row) != $LINES:
-        print "Error, field count at line #%d: %s" % (i, row)
+        writeout("%sError, field count at line #%d: %s\n" % (RED, i, RESET))
+        writeout("%s\n" % delim_char.join(row))
         exit(1)
     if i > 1:
         for f in range(min(len(row), len(rec))):
             if not rec[f].match(row[f]):
-                print "Error, field match at line #%d, field #%d: %s" % (i, f+1, row)
+                s = max(0, f-1)
+                e = f+1
+                pre = delim_char.join(row[:s]) + (delim_char if len(row[:s]) else '')
+                post = (delim_char if len(row[e:]) else '') + delim_char.join(row[e:])
+                writeout("%sError, field validation at line #%d, field #%d: %s\n" % (RED, i, f+1, RESET))
+                writeout("%s%s%s%s%s\n" % (pre, RED, row[f], RESET, post))
                 exit(1)
     i += 1
 EOF
 python -c "$CMD" < /dev/stdin
-[ "$?" -ne 0 ] || {
+[ "$?" -ne 0 ] && {
     exit 1
+} || {
+    exit 0
 }
